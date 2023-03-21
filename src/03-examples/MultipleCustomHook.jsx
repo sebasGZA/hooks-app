870563dd05cfeefdc07f668a8eb250e3fd784c60
sebasGZA <sebassga34@gmail.com>
2023-03-21 15:15:21 -0500

    Optimize last code

--- src/03-examples/MultipleCustomHook.jsx
+++ src/03-examples/MultipleCustomHook.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { useFetch } from "../hooks/useFetch";
-import { useCounter } from "../hooks/useCounter";
+import { useFetch, useCounter } from "../hooks";
+import { Quote, LoadingQuote } from "./";
 
 export const MultipleCustomHook = () => {
   const { counter, increment } = useCounter(1);
@@ -23,14 +23,7 @@ export const MultipleCustomHook = () => {
       <h1>Breaking Bad Quotes</h1>
       <hr />
 
-      {isLoading ? (
-        <div className="alert alert-info text-center">Loading...</div>
-      ) : (
-        <blockquote className="blockquote text-end">
-          <p className="mb-1">{quote}</p>
-          <footer className="blockquote-footer">{author}</footer>
-        </blockquote>
-      )}
+      {isLoading ? <LoadingQuote /> : <Quote quote={quote} author={author} />}
 
       <button
         className="btn btn-primary"
