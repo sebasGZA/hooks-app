88c978ec1fada4a0df997a784c0f835533d610f6
sebasGZA <sebassga34@gmail.com>
2023-03-21 14:58:04 -0500

    Hooks: useFetch + useCouter custom hooks

--- src/03-examples/MultipleCustomHook.jsx
+++ src/03-examples/MultipleCustomHook.jsx
@@ -6,11 +6,26 @@ export const MultipleCustomHook = () => {
     "https://api.breakingbadquotes.xyz/v1/quotes/1"
   );
 
-  console.log({ data, isLoading, hasError });
-  
+  const { author, quote } = !!data && data[0];
+  //   if (isLoading) {
+  //     return <h1>Loading...</h1>;
+  //   }
+
   return (
     <>
       <h1>Breaking Bad Quotes</h1>
+      <hr />
+
+      {isLoading ? (
+        <div className="alert alert-info text-center">Loading...</div>
+      ) : (
+        <blockquote className="blockquote text-end">
+          <p className="mb-1">{quote}</p>
+          <footer className="blockquote-footer">{author}</footer>
+        </blockquote>
+      )}
+
+      <button className="btn btn-primary">Next Quote</button>
     </>
   );
 };
