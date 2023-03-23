ea3f354683299bdde0ba81d5c9343239d70b8cdd
sebasGZA <sebassga34@gmail.com>
2023-03-23 10:24:00 -0500

    Hooks: useCallBack with arguments

--- src/07-callbacks/CallBackHook.jsx
+++ src/07-callbacks/CallBackHook.jsx
@@ -1,12 +1,16 @@
-import { useState, useCallback } from "react";
+import { useState, useCallback, useEffect } from "react";
 import { ShowIncrement } from "./ShowIncrement";
 
 export const CallBackHook = () => {
   const [counter, setCounter] = useState(0);
 
-  const increment = useCallback(() => {
-    setCounter((count) => count + 1);
+  const increment = useCallback((value = 1) => {
+    setCounter((count) => count + value);
   }, []);
+
+  // useEffect(() => {
+  //   increment();
+  // }, [increment]);
 
   return (
     <>
--- src/07-callbacks/ShowIncrement.jsx
+++ src/07-callbacks/ShowIncrement.jsx
@@ -7,7 +7,7 @@ export const ShowIncrement = memo(({ increment }) => {
     <button
       className="btn btn-primary"
       onClick={() => {
-        increment();
+        increment(5);
       }}
     >
       Increment
