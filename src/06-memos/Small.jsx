edf769b32f9dce0cdf609fb8fae5c510f8fe3977
sebasGZA <sebassga34@gmail.com>
2023-03-23 09:36:30 -0500

    Hooks: memo

---
+++ src/06-memos/Small.jsx
@@ -0,0 +1,6 @@
+import { memo } from "react";
+
+export const Small = memo(({ value }) => {
+  console.log(" Me volvi a generar ");
+  return <small>{value}</small>;
+});
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { Layout } from "./05-useLayoutEffect/Layout";
+import { Memorize } from "./06-memos/Memorize";
 
 // import { HooksApp } from "./HooksApp";
 // import { CounterApp } from "./01-useState/CounterApp";
@@ -10,6 +10,7 @@ import { Layout } from "./05-useLayoutEffect/Layout";
 // import { FormWithCustomHook } from "./02-useEffect/FormWithCustomHook";
 // import { MultipleCustomHook } from "./03-examples/MultipleCustomHook";
 // import { FocusScreen } from "./04-useRef/FocusScreen";
+// import { Layout } from "./05-useLayoutEffect/Layout";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
@@ -19,6 +20,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   // {/* <SimpleForm /> */}
   // {/* <FormWithCustomHook /> */}
   // <MultipleCustomHook />
-  <Layout />
+  <Memorize/>
   // {/* </React.StrictMode> */}
 );
