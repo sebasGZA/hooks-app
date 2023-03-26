2c530868b1d87b5b3b6660e58859595f13f061ab
sebasGZA <sebassga34@gmail.com>
2023-03-26 14:56:50 -0500

    Test: Call Events with TodoItem

--- src/tests/09-useReducer/TodoItem.test.jsx
+++ src/tests/09-useReducer/TodoItem.test.jsx
@@ -1,8 +1,8 @@
-const { render, screen } = require("@testing-library/react");
+const { render, screen, fireEvent } = require("@testing-library/react");
 const { TodoItem } = require("../../09-useReducer/TodoItem");
 
 describe("Test in <TodoItem />", () => {
-  const todo = {
+  let todo = {
     id: 1,
     description: "Rock of the soul",
     done: false,
@@ -34,5 +34,60 @@ describe("Test in <TodoItem />", () => {
 
     const spanElement = screen.getByLabelText("span");
     expect(spanElement.className).toContain("align-self-center");
+  });
+
+  test("Should show the component completed", () => {
+    todo.done = true;
+
+    render(
+      <TodoItem
+        key={todo.id}
+        todo={todo}
+        onToggleTodo={onToggleTodoMock}
+        onRemoveTodo={onRemoveTodoMock}
+      />
+    );
+
+    const liElement = screen.getByRole("listitem");
+    expect(liElement.className).toBe(
+      "list-group-item d-flex justify-content-between"
+    );
+
+    const spanElement = screen.getByLabelText("span");
+    expect(spanElement.className).toBe(
+      "align-self-center text-decoration-line-through"
+    );
+  });
+
+  test("Should call the toggle todo when it is double-clicked", () => {
+    render(
+      <TodoItem
+        todo={todo}
+        onToggleTodo={onToggleTodoMock}
+        onRemoveTodo={onRemoveTodoMock}
+      />
+    );
+
+    const spanElement = screen.getByLabelText("span");
+    fireEvent.doubleClick(spanElement);
+
+    expect(onToggleTodoMock).toHaveBeenCalled();
+    expect(onToggleTodoMock).toHaveBeenCalledWith(todo.id);
+  });
+
+  test("Should call the remove todo when it is clicked", () => {
+    render(
+      <TodoItem
+        todo={todo}
+        onToggleTodo={onToggleTodoMock}
+        onRemoveTodo={onRemoveTodoMock}
+      />
+    );
+
+    const buttonElement = screen.getByRole("button");
+    fireEvent.click(buttonElement);
+
+    expect(onRemoveTodoMock).toHaveBeenCalled();
+    expect(onRemoveTodoMock).toHaveBeenCalledWith(todo.id);
   });
 });
